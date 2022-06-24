cf87bba63b0916278839639c957e44c76c09abc2
Nicolas Castejon <ncastejon.externe@bedrockstreaming.com>
2022-06-24 11:22:05 +0200

    Add simple lobby

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,23 +1,99 @@
-import { useFirestore, useFirestoreCollectionData } from "reactfire";
+import { addDoc, collection, deleteDoc, doc, setDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { auth, db } from "../..";
 
 export default function Lobby() {
-  //   const lobbyCollection = useFirestore().collection("lobby");
-  //   const lobby = useFirestoreCollectionData(lobbyCollection);
+  const [user] = useAuthState(auth);
+  const [userInLobby, setUserInLobby] = useState(false);
+  const [isPlayerReady, setPlayerReady] = useState(false);
+
+  const lobbyRef = collection(db, "lobby");
+  const [lobby, loading, error] = useCollection(lobbyRef, {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+
+  // When the component is unmounted, leave the lobby
+  useEffect(() => {
+    return () => {
+      deleteDoc(doc(db, "lobby", user.uid));
+    };
+  }, [user.uid]);
+
+  // Same when the tab is closed
+  useEffect(() => {
+    const handleTabClose = () => {
+      deleteDoc(doc(db, "lobby", user.uid));
+    };
+    window.addEventListener("beforeunload", handleTabClose);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleTabClose);
+    };
+  }, [user.uid]);
+
+  useEffect(() => {
+    const player =
+      lobby && lobby.docs.find((p) => p.data().email === user.email);
+    if (player) {
+      setUserInLobby(player.data());
+    }
+  }, [lobby, user.email]);
+
+  const joinLobby = async () => {
+    await setDoc(doc(db, "lobby", user.uid), {
+      email: user.email,
+      displayName: user.displayName,
+      isReady: false,
+    });
+  };
+
+  const changeReadiness = async (newReadiness) => {
+    await setDoc(
+      doc(db, "lobby", user.uid),
+      {
+        isReady: newReadiness,
+      },
+      { merge: true }
+    );
+  };
+
+  const leaveLobby = async () => {
+    await deleteDoc(doc(db, "lobby", user.uid));
+  };
 
   return (
     <div>
       <h1>Lobby</h1>
-      {/* <div>
-        {lobby.map((player) => {
-          return (
-            <article key={player.email}>
-              <p>
-                {player.displayName} - {player.ready ? "Prêt" : "Pas prêt"}
-              </p>
-            </article>
-          );
-        })}
-      </div> */}
+
+      {lobby && (
+        <div>
+          {lobby.docs.map((l) => {
+            const player = l.data();
+            return (
+              <article key={player.email}>
+                <p>
+                  {player.displayName} - {player.isReady ? "Prêt" : "Pas prêt"}
+                </p>
+              </article>
+            );
+          })}
+        </div>
+      )}
+      <div>
+        {userInLobby ? (
+          <>
+            <button onClick={leaveLobby}>Quitter</button>
+            <button onClick={() => changeReadiness(!userInLobby.isReady)}>
+              {" "}
+              {userInLobby.isReady ? "Pas prêt" : "Prêt!"}{" "}
+            </button>
+          </>
+        ) : (
+          <button onClick={joinLobby}>Rejoindre</button>
+        )}
+      </div>
     </div>
   );
 }
