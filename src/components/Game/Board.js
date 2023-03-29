1be9ffb5b092b21e5123154d1dd4b9f844521ea1
Nicolas Castejon <nicolas.castejon@protonmail.com>
2023-03-29 21:14:20 +0200

    Create a basic field with position for cards

--- src/components/Game/Board.js
+++ src/components/Game/Board.js
@@ -1,65 +1,70 @@
-
-import { GizmoHelper, GizmoViewport, OrbitControls, Plane } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { GizmoHelper, GizmoViewport, OrbitControls, Plane, Text } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
 import { useMemo } from "react";
 import { useControls } from 'leva'
+import * as THREE from "three"
 
 const cardHeight = 1;
 const cardWidth = 0.5;
 const gapHeight = 0.2;
-const fieldHeight = cardHeight * 6 + (gapHeight * 4 ) + 0.3
-const fieldWidth = cardWidth * 12
+const fieldHeight = (cardHeight * 6 + (gapHeight * 4) + 0.3) * 6
+const fieldWidth = (cardWidth * 12 + (gapHeight * 10) + 0.3) * 6
+const planePadding = 0.1
 
-function CardField({ position }) {
+function CardField({ position, index, color }) {
     // const cardTexture = useTexture('path/to/cardTexture.png');
-    const { cardRotation, color, visible } = useControls('cards', {
+    const { cardRotation } = useControls('cards', {
 
         cardRotation: {
             value: [0, 0, 0],
             step: 0.1,
             joystick: 'invertY'
-        },
-        color: "#5348be",
-        visible: true
+        }
     })
 
     return (
         <Plane args={[cardWidth, cardHeight]} position={position} rotation={cardRotation}>
+            <Text scale={0.2} anchorX="center" anchorY="middle" position-z={0.2} color="black">{index}</Text>
             <meshBasicMaterial color={color} />
         </Plane>
     );
 }
 
+function FogEffect() {
+    const { scene } = useThree();
+    scene.fog = new THREE.Fog("white", 25, 30);
+    return null;
+  }
+
 export default function Board({ gameStore }) {
-    const leftField = useMemo(() => {
-        const leftCards = [];
-        for (let i = 0; i < 2; i++) {
-            for (let j = 0; j < 6; j++) {
-                leftCards.push(<CardField key={`left-${i}-${j}`} position={[i, j + 0.2 ,0.2]} />);
+    function generateCardFields(startColumn, endColumn, isIndexed = false, cardColor = undefined) {
+        const cards = [];
+        let cardIndex = 1;
+        for (let row = 0; row < 6; row++) {
+            for (let column = startColumn; column < endColumn; column++) {
+                const xPos = column * (cardWidth + gapHeight) - fieldWidth / 12 + cardWidth / 2 + planePadding;
+                const yPos = row * (cardHeight + gapHeight) - fieldHeight / 12 + cardHeight / 2 + planePadding;
+                cards.push(
+                    <CardField
+                        key={`${column}-${row}`}
+                        position={[xPos, yPos, 0.1]}
+                        index={isIndexed ? cardIndex : undefined}
+                        color={cardColor}
+                    />
+                );
+                if (isIndexed) {
+                    cardIndex++;
+                }
             }
         }
-        return leftCards;
-    }, []);
-
-    const middleField = useMemo(() => {
-        const middleCards = [];
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 9; j++) {
-                middleCards.push(<CardField key={`middle-${i}-${j}`} position={[i + 3, 0, j]} />);
-            }
-        }
-        return middleCards;
-    }, []);
-
-    const rightField = useMemo(() => {
-        const rightCards = [];
-        for (let i = 0; i < 2; i++) {
-            for (let j = 0; j < 6; j++) {
-                rightCards.push(<CardField key={`right-${i}-${j}`} position={[i + 9, 0, j]} />);
-            }
-        }
-        return rightCards;
-    }, []);
+        return cards;
+    }
+    
+    const leftField = useMemo(() => generateCardFields(0, 2, true), []);
+    const flancCoco = useMemo(() => generateCardFields(2, 3, false, "black"), []);
+    const middleField = useMemo(() => generateCardFields(3, 7, true), []);
+    const flancPomme = useMemo(() => generateCardFields(7, 8, false, "black"), []);
+    const rightField = useMemo(() => generateCardFields(8, 11, true), []);
 
     const { planeRotation, planePosition, color, visible } = useControls('field', {
         planePosition: {
@@ -72,8 +77,21 @@ export default function Board({ gameStore }) {
             step: 0.1,
             joystick: 'invertY'
         },
-        color: "#c179c1",
+        color: "#6db766",
         visible: true
+    })
+
+    const { groupRotation, groupPosition } = useControls('group', {
+        groupPosition: {
+            value: [0, 0.5, 0],
+            step: 0.1,
+            joystick: 'invertY'
+        },
+        groupRotation: {
+            value: [-0.5, 0, 0],
+            step: 0.1,
+            joystick: 'invertY'
+        },
     })
 
     return (
@@ -94,11 +112,18 @@ export default function Board({ gameStore }) {
                 {/* alternative: <GizmoViewcube /> */}
             </GizmoHelper>
             <ambientLight />
-            <Plane args={[fieldWidth, fieldHeight]}
-                visible={visible}
-                material-color={color}
-                position={planePosition} rotation={planeRotation} ma />
+            <FogEffect />
+            <group position={groupPosition} rotation={groupRotation}>
+                <Plane args={[fieldWidth, fieldHeight]}
+                    visible={visible}
+                    material-color={color}
+                    position={planePosition} rotation={planeRotation} />
                 {leftField}
+                {flancCoco}
+                {middleField}
+                {flancPomme}
+                {rightField}
+            </group>
         </Canvas>
 
     );
